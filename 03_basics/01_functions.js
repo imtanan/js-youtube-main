62819d3f13a4f3dd7edff9032773a0c6d64ab5b7
imtanan <52236287+imtanan@users.noreply.github.com>
2025-10-08 09:20:38 +0000

    func

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,24 @@
+function sayMyName(){
+    console.log("i");
+    console.log("m");
+    console.log("r");
+    console.log("a");
+    console.log("n");
+    
+}
+//sayMyName()
+function addTwoNumbers(number1,number2){
+    const result1= number1+number2
+    
+    console.log("hitesh");
+    return result1
+}
+const result = addTwoNumbers(1,2)
+
+console.log("Result: ", result);
+function loginUserMessage(uername){
+        return `${username} just logged in`
+}
+console.log(loginUserMessage("Hitesh"));
+
+
