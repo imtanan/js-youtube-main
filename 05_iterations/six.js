11c9851fdb30277fbc5124e557275762a2ec535d
imtanan <52236287+imtanan@users.noreply.github.com>
2025-10-13 06:05:19 +0000

    sevenandsixthchanges

---
+++ 05_iterations/six.js
@@ -0,0 +1,12 @@
+//const coding = ["js","ruby","java","python","cpp"]
+//const value = coding.forEach((item)=>{
+  // console.log(item);
+  //return item
+   
+//})
+//console.log(value);
+
+const myNums = [1,2,3,4,5,6,7,8,9,10]
+const numbers = myNums.filter((item)=> item>4)
+console.log(numbers);
+
