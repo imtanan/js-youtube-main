3e5d1c1007f5ef0f5f21ccbfa008c50b61eb0b00
imtanan <52236287+imtanan@users.noreply.github.com>
2025-10-23 19:03:22 +0000

    promisesandoop

---
+++ 10_classes_and_oop/oop.js
@@ -0,0 +1,22 @@
+/*const user = {
+  username: "imtanan",
+  loginCount: 8,
+  signedIn: true,
+
+  getUserDetails: function () {
+    console.log(`username:  ${this.username}`);
+    console.log(this);
+  },
+};
+console.log(user.getUserDetails());*/
+function User(username, loginCount, isLoggedIn) {
+  this.username = username;
+  this.loginCount = loginCount;
+  this.isLoggedIn = isLoggedIn;
+  return this;
+}
+const userOne = User("imtanan", 8, true);
+const userTwo = User("Kajal", 12, false);
+
+console.log(userOne);
+console.log(userTwo);
